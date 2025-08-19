ccdfd1616d7490e8093fd3b1a2d50c8eb5d3c67b
Ori Shadmon <oshadmon@gmail.com>
2025-08-18 22:26:28 -0700

    data generator
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- data_generator.py
+++ data_generator.py
@@ -15,27 +15,27 @@ METADATAS = {
         "oil_pressure_kpa": 27,
         "oil_temp_cooled_c": 42
     },
-    # "main loop": {
-    #     "file": os.path.join('base_examples', 'hpu_mud_system_main_loop_data.v6.csv'),
-    #     "main_loop_flowrate_lpm": 19,
-    #     "discharge_pressure_1_kpa": 24,
-    #     "discharge_pressure_2_kpa": 25
-    # },
-    # "thermocouples": {
-    #     "file": os.path.join('base_examples', 'hpu_mud_system_thermocouple_data.v6.csv'),
-    #     'TC1001': 31,
-    #     'TC1002': 32,
-    #     'TC1003': 33,
-    #     'TC1004': 34,
-    #     'TC1005': 35,
-    #     'TC1006': 36,
-    #     'TC1007': 37,
-    #     'TC1008': 38,
-    #     'TC1009': 39,
-    #     'TC1010': 40,
-    #     'TC1011': 41,
-    #     'TC1012': 42
-    # }
+    "main loop": {
+        "file": os.path.join('base_examples', 'hpu_mud_system_main_loop_data.v6.csv'),
+        "main_loop_flowrate_lpm": 19,
+        "discharge_pressure_1_kpa": 24,
+        "discharge_pressure_2_kpa": 25
+    },
+    "thermocouples": {
+        "file": os.path.join('base_examples', 'hpu_mud_system_thermocouple_data.v6.csv'),
+        'TC1001': 31,
+        'TC1002': 32,
+        'TC1003': 33,
+        'TC1004': 34,
+        'TC1005': 35,
+        'TC1006': 36,
+        'TC1007': 37,
+        'TC1008': 38,
+        'TC1009': 39,
+        'TC1010': 40,
+        'TC1011': 41,
+        'TC1012': 42
+    }
 }
 
 
@@ -55,16 +55,43 @@ def post_data(conn: str, payload: list):
         raise Exception(f"POST failed: {error}")
 
 
+def update_timestamp(file_content: list):
+    if not file_content:
+        return file_content
+
+    # Parse all original timestamps into datetime objects
+    original_ts = [
+        datetime.datetime.strptime(row['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        for row in file_content
+    ]
+
+    # Start new series 1 minute after the last timestamp
+    base_start_ts = original_ts[-1] + datetime.timedelta(minutes=1)
+
+    # Rebuild timestamps using original increments
+    new_ts = [base_start_ts]
+    for i in range(1, len(original_ts)):
+        diff = original_ts[i] - original_ts[i - 1]
+        new_ts.append(new_ts[-1] + diff)
+
+    # Assign back into file_content
+    for i in range(len(file_content)):
+        file_content[i]['timestamp'] = new_ts[i].strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+    return file_content
+
 def csv2json(file_path: str):
     if os.path.isfile(file_path):
         df = pd.read_csv(file_path)
         return json.loads(df.to_json(orient="records", lines=False))
 
 
-def update_timestamp(file_content: list, iteration: int):
+def update_timestamp_orig(file_content: list):
     min_ts = None
     max_ts = None
 
+    for i in range(len(file_content)):
+        ts = datetime.datetime.strptime(row['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
     for row in file_content:
         ts = datetime.datetime.strptime(row['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
         if not min_ts:
@@ -73,11 +100,12 @@ def update_timestamp(file_content: list, iteration: int):
             max_ts = ts
 
     time_diff = max_ts - min_ts
-    increments = (time_diff.total_seconds() / max(1, len(file_content) - 1)) + (iteration * 60)
+    base_start_ts = max_ts + datetime.timedelta(minutes=1)
+    increments = (time_diff.total_seconds() / max(1, len(file_content) - 1))
 
     # Assign new timestamps in order
-    for i, row in enumerate(file_content):
-        row['timestamp'] = (max_ts + datetime.timedelta(seconds=increments)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+    for i in range(len(file_content)):
+        file_content[i]['timestamp'] = (base_start_ts + datetime.timedelta(seconds=(increments * i))).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
     return file_content
 
@@ -106,7 +134,7 @@ def worker(metadata: str, iteration: int):
     print(f"[Thread] {metadata} iteration {iteration}")
     file_content = csv2json(file_path=METADATAS[metadata]['file'])
     if iteration > 0:
-        file_content = update_timestamp(file_content, iteration=iteration)
+        file_content = update_timestamp(file_content)
     # print(file_content[0]['timestamp'])
     publish_data(metadata=metadata, file_content=file_content)
 
